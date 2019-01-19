693ced946114da001ba19a52e7a0b3270eb422fd
zyc1310517843 <37543038+zyc1310517843@users.noreply.github.com>
2019-01-19 15:22:41 +0800

    Update DocVwcText.py

--- DocVwcText.py
+++ DocVwcText.py
@@ -18,26 +18,26 @@ import jieba
 #     wf.write(result+'\n')
 # wf.close()
 
-# rf = open('word2vec/new.txt', 'r', encoding='utf-8')
-# wf = open('trainData.txt', 'a+', encoding='utf-8')
-# lines = rf.readlines()
-# for line in lines:
-#     document = line.replace('<p>', '').replace('</p>', '').replace('\n', '').replace('，', '').replace('。',
-#                                                                                                           '').replace(
-#         '：', '').replace('？', '').replace(' ', '').replace('"summarization":', '').replace('"article"', '').replace('{', '').replace('}', '').replace(':', '').replace('<Paragraph>', '')
-#     document_cut = jieba.cut(document, cut_all=False)
-#     result = ' '.join(document_cut)
-#     wf.write(result+'\n')
+rf = open('new.txt', 'r', encoding='utf-8')  # 自己的需要训练的文档集合，一行一个文章或者句子
+wf = open('trainData.txt', 'a+', encoding='utf-8')
+lines = rf.readlines()
+for line in lines:
+    document = line.replace('<p>', '').replace('</p>', '').replace('\n', '').replace('，', '').replace('。',
+                                                                                                          '').replace(
+        '：', '').replace('？', '').replace(' ', '').replace('"summarization":', '').replace('"article"', '').replace('{', '').replace('}', '').replace(':', '').replace('<Paragraph>', '')
+    document_cut = jieba.cut(document, cut_all=False)
+    result = ' '.join(document_cut)
+    wf.write(result+'\n')
 
 
-sentences = gensim.models.doc2vec.TaggedLineDocument('tempTrain.txt')
+sentences = gensim.models.doc2vec.TaggedLineDocument('tempTrain.txt')  #  训练
 model = gensim.models.Doc2Vec(sentences, vector_size=256, window=2)
 model.train(sentences, total_examples=model.corpus_count, epochs=model.epochs)
 model.save('all_model.txt')
 
 
-print(len(model.docvecs))
-sims = model.docvecs.most_similar(0)
+print(len(model.docvecs))  #  所有文章的向量
+sims = model.docvecs.most_similar(0)  #输出和训练集中第一个文章最相近的文章
 print(sims)
 
 sims1 = model.docvecs.most_similar(14)
